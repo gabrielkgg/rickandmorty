b5a9c5c8d75416a1399e45c5d83e5ee60a7f1c16
Gabriel Alves <gabriel@meucrediario.com.br>
2021-07-19 21:16:42 -0300

    Implementada a busca dinamica, criado cards de characters

---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="character-card">
+        <img class="character-card-image" :src="character.image" />
+        <div class="character-card-tag">
+            <div class="character-card-name">{{ character.name }}</div>
+            <div class="character-card-species">{{ character.species }}</div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Options } from 'vue-class-component'
+import { Prop } from '../decorators/prop.decorator'
+import { CharacterDto } from '../dto/character.dto'
+
+@Options({})
+export default class CharacterCard extends Vue {
+    @Prop() character: CharacterDto
+}
+</script>
+
+<style lang="scss">
+@import '../sass/_all.scss';
+
+.character-card {
+    position: relative;
+    border: 2px solid $color-gray-2;
+    border-radius: $border-radius;
+    overflow: hidden;
+    margin-bottom: 2em;
+    cursor: pointer;
+    transition: 1s;
+
+    &-image {
+        -webkit-filter: grayscale(100%);
+        filter: grayscale(100%);
+    }
+
+    &-tag {
+        width: 100%;
+        padding: 0.25em 0.5em;
+        background-color: #040404c2;
+        backdrop-filter: blur(6px);
+        position: absolute;
+        bottom: 0;
+        z-index: 1;
+    }
+
+    &-name {
+        font-size: 28px;
+        font-weight: 700;
+    }
+
+    &-species {
+        font-size: 16px;
+    }
+
+    &:hover {
+        border-color: var(--color-yellow);
+        box-shadow: 0 2px 20px var(--color-yellow);
+
+        .character-card-image {
+            -webkit-filter: grayscale(0%);
+            filter: grayscale(0%);
+        }
+    }
+}
+</style>
---
+++ src/components/CharacterList.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container flex wrap justify-space-between">
+        <div v-for="(character, index) in list" :key="index">
+            <CharacterCard :character="character" />
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Options } from 'vue-class-component'
+import { Prop } from '../decorators/prop.decorator'
+import { CharacterDto } from '../dto/character.dto'
+import CharacterCard from './CharacterCard.vue'
+
+@Options({
+    components: {
+        CharacterCard,
+    },
+})
+export default class CharacterList extends Vue {
+    @Prop() list: CharacterDto[]
+}
+</script>
+
+<style lang="scss">
+@import '../sass/_all.scss';
+</style>
